39a4d00f7b2a88a83d17429015bc323d144126d8
newaz sourav <nsourov07@gmail.com>
2025-08-23 00:00:45 +0600

    updated port

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const path = require("path");
+
+// Routers
 const ownerRouter = require("./routes/ownerRouter");
 const userRouter = require("./routes/userRouter");
 const productsRouter = require("./routes/productsRouter");
 const indexRouter = require("./routes/indexRouter");
 const orderRouter = require("./routes/orderRouter");
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: true, 
-  credentials: true, 
-}));
-
+// Database
 const db = require("./config/mongoose_connection");
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 db();
 
+// Middleware
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-//manage routerss
-app.use("/",indexRouter);
-app.use("/owner",ownerRouter);
-app.use("/user",userRouter);
-app.use("/products",productsRouter);
-app.use("/order",orderRouter);
+// Routes
+app.use("/", indexRouter);
+app.use("/owner", ownerRouter);
+app.use("/user", userRouter);
+app.use("/products", productsRouter);
+app.use("/order", orderRouter);
 
+// Listen on Render's port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
