df87eb44d7b6670636439990751f794c2e6aa66b
newaz sourav <nsourov07@gmail.com>
2025-08-30 16:02:48 +0600

    Some updates

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userModel = require("../models/userModel");
-const productModel = require("../models/productModel");
-const bcrypt = require("bcrypt");
-const { generateToken } = require("../utils/generateToken");
+const orderModel = require("../models/orderModel");
+const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {generateToken}=require("../utils/generateToken");
 const isloggedin = require("../middlewares/isloggedin");
+const productModel = require("../models/productModel");
+require("dotenv").config();
 const cache = require("../controllers/cache"); // shared cache
 
 // Get user profile
@@ -13,6 +16,19 @@ router.get("/profile", isloggedin, async (req, res) => {
     const user = await userModel.findById(req.user._id).select("-password");
     if (!user) return res.status(404).send("User not found");
     res.status(200).json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.get("/orders", isloggedin, async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ user: req.user._id })
+      .populate("items.product", "name image price") // get product details
+      .sort({ orderDate: -1 }); // newest first
+
+    res.status(200).json(orders);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -45,6 +61,94 @@ router.get("/cart", isloggedin, async (req, res) => {
     res.status(500).send(err.message);
   }
 });
+
+router.post("/register",async function(req,res){
+
+   try{
+     let {fullname, email, password}=req.body;
+
+     let user =await userModel.findOne({email:email});
+
+     if(user){
+        return res.status(401).send("Already Registered");
+     }
+
+        bcrypt.genSalt(10, function(err, salt) {         
+        bcrypt.hash(password, salt,async function(err, hash) {
+           
+            if(err){
+                res.status(500).send(err.message);
+            }
+
+        let newUSer = await userModel.create({
+        fullname,
+        email,
+        password:hash,
+    });
+
+    let token=generateToken(newUSer);
+    res.cookie('token', token, {
+                 httpOnly: true,
+                  secure: true,      
+                  sameSite: 'None',
+                  maxAge: 7 * 24 * 60 * 60 * 1000      
+                  });
+    res.status(200).send("User Created succesfully");
+        });
+    });
+
+    
+   }
+   catch(err)
+   {
+    res.status(500).send(err.message);
+   }
+});
+
+router.post("/login", async function(req,res)
+{
+    try{
+        let {email,password}=req.body;
+    const user = await userModel.findOne({email:email});
+
+    if(!user)
+    {
+        return res.status(501).send("Invalid email or password");
+    }
+
+    bcrypt.compare(password,user.password, function(err,result){
+        if(result)
+        {
+            let token = generateToken(user);
+            res.cookie('token', token, {
+                 httpOnly: true,
+                  secure: true,      
+                  sameSite: 'None',
+                  maxAge: 7 * 24 * 60 * 60 * 1000      
+                  });
+            res.send("Logged in");
+        }
+
+        else{
+             return res.status(501).send("Invalid email or password");
+        }
+    })
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+})
+
+router.post("/logout", function(req,res)
+{
+    res.clearCookie('token', {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true, 
+  });
+    res.send("Logged Out");
+})
+
+
 
 // Add to cart
 router.post("/addtocart/:id", isloggedin, async (req, res) => {
