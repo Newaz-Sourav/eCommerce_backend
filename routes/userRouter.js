584213b9c3e9675508ea3ee9eb18bc04666d0ecd
newaz sourav <nsourov07@gmail.com>
2025-08-30 18:17:20 +0600

    Some updates

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -35,34 +35,28 @@ router.get("/orders", isloggedin, async (req, res) => {
 });
 
 // Get user's cart
-// Get user's cart
 router.get("/cart", isloggedin, async (req, res) => {
   try {
     const userEmail = req.user.email;
+
+    const cachedCart = cache.get(userEmail);
+    if (cachedCart) return res.status(200).json(cachedCart);
+
     const user = await userModel.findOne({ email: userEmail }).populate("cart.product", "name image price");
     if (!user) return res.status(404).send("User not found");
 
-    // Combine duplicates
-    const cartMap = new Map();
-    user.cart.forEach(item => {
-      const id = item.product._id.toString();
-      if (cartMap.has(id)) {
-        const existing = cartMap.get(id);
-        existing.quantity += item.quantity;
-      } else {
-        cartMap.set(id, {
-          _id: id,
-          name: item.product.name,
-          image: item.product.image,
-          price: item.price || item.product.price,
-          quantity: item.quantity
-        });
-      }
-    });
+    const cartData = user.cart.map(item => ({
+      _id: item.product._id,
+      name: item.product.name,
+      image: item.product.image,
+      quantity: item.quantity,
+      price: item.price || item.product.price
+    }));
 
-    const cartData = Array.from(cartMap.values());
+    const response = { cart: cartData, cartTotal: user.cartTotal };
+    cache.set(userEmail, response);
+    res.status(200).json(response);
 
-    res.status(200).json({ cart: cartData, cartTotal: user.cartTotal });
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -195,39 +189,27 @@ router.post("/removefromcart/:id", isloggedin, async (req, res) => {
   try {
     const productId = req.params.id;
     const userEmail = req.user.email;
-    const removeCompletely = req.body.removeCompletely === true; // strict boolean
+    const removeCompletely = req.body.removeCompletely || false;
 
     const user = await userModel.findOne({ email: userEmail }).populate("cart.product");
     if (!user) return res.status(404).send("User not found");
 
-    const index = user.cart.findIndex(item => item.product._id.toString() === productId.toString());
-
+    const index = user.cart.findIndex(item => item.product._id.toString() === productId);
     if (index > -1) {
-      if (removeCompletely) {
-        // remove completely regardless of quantity
+      if (removeCompletely || user.cart[index].quantity <= 1) {
         user.cart.splice(index, 1);
       } else {
-        // decrease quantity by 1
         user.cart[index].quantity -= 1;
-        if (user.cart[index].quantity <= 0) {
-          user.cart.splice(index, 1);
-        }
       }
 
-      // recalc total
-      user.cartTotal = user.cart.reduce(
-        (sum, item) => sum + (item.price || item.product.price) * item.quantity,
-        0
-      );
-
+      user.cartTotal = user.cart.reduce((sum, item) => sum + (item.price || item.product.price) * item.quantity, 0);
       await user.save();
       cache.del(userEmail);
 
-      return res.status(200).json({ message: "Product removed from cart successfully", cart: user.cart, cartTotal: user.cartTotal });
+      res.status(200).json({ message: "Product removed from cart successfully", cart: user.cart, cartTotal: user.cartTotal });
     } else {
-      return res.status(404).send("Product not found in cart");
+      res.status(404).send("Product not found in cart");
     }
-
   } catch (err) {
     res.status(500).send(err.message);
   }
